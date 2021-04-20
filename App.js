ad559b2f7f1cb3ac40cb40ed34c3b56473b0ce5e
Hewr <hewr_s@outlook.com>
2021-04-21 02:07:12 +0300

    lesson-3

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import AppText from './app/components/AppText/AppText';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
-const App = () => {
-  return <ViewImageScreen />;
-};
-
+const App = () => (
+  <View styles={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <AppText>I Love RN</AppText>
+  </View>
+);
 export default App;
---
+++ app/components/AppText/styles.js
@@ -0,0 +1,9 @@
+import { Platform, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
+  },
+});
+export default styles;
